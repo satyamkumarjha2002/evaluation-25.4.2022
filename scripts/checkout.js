2c458dcc77581dfb477fd86deaf3173dcc610dde
Satyam Kumar Jha <87626810+satyamkumarjha2002@users.noreply.github.com>
2022-04-25 11:45:17 +0530

    some correction

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,39 +3,41 @@
 // Else show "Booking successfull!" and adjust the wallet amount in real time
 
 let wallet_amount = JSON.parse(localStorage.getItem("amount"));
-let data=document.querySelector("#wallet");
+let data = document.querySelector("#wallet");
 data.innerText = wallet_amount;
 
 function book() {
     let seats = document.querySelector("#number_of_seats").value;
-    let total_amount=Number(seats*100)
-    if(total_amount<=wallet_amount){
-        let wallet_amou = JSON.parse(localStorage.getItem("amount"));
-        alert('Booking successfull!');
-        let after_wallet=(Number(wallet_amou)-Number(total_amount));
+    let total_amount = Number(seats * 100);
+    let wallet_amou = JSON.parse(localStorage.getItem("amount"));
+    let after_wallet = (Number(wallet_amou) - Number(total_amount));
+    if (total_amount <= wallet_amou && wallet_amou > 0) {
         setlocl(after_wallet)
-    }else{
+        alert('Booking successfull!');
+    } else {
         alert("Insufficient Balance!");
     }
 
 }
-function setlocl(after_wallet){
-    localStorage.setItem("amount",after_wallet);
-    data.innerText=after_wallet;
+
+function setlocl(after_wallet) {
+    localStorage.setItem("amount", after_wallet);
+    data.innerText = after_wallet;
 }
-function display(){
-    let data=JSON.parse(localStorage.getItem("movie"));
-    let main_div=document.querySelector("#movie");
-    let div=document.createElement("div");
-    let div1=document.createElement("div");
-    let image=document.createElement("img");
-    image.src=data.Poster;
+
+function display() {
+    let data = JSON.parse(localStorage.getItem("movie"));
+    let main_div = document.querySelector("#movie");
+    let div = document.createElement("div");
+    let div1 = document.createElement("div");
+    let image = document.createElement("img");
+    image.src = data.Poster;
     div1.append(image);
-    let div2=document.createElement("div");
-    let title=document.createElement("p");
-    title.innerText=data.Title;
+    let div2 = document.createElement("div");
+    let title = document.createElement("p");
+    title.innerText = data.Title;
     div2.append(title);
-    div.append(div1,div2);
+    div.append(div1, div2);
     main_div.append(div);
 
 }
